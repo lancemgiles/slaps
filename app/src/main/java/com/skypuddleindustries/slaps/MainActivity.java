205dde32f2dc8b02cc4ab5e50ff32db1c36bac23
greze <lmgiles6@gmail.com>
2020-05-10 12:47:51 -0400

    step four: handling audio focus

--- app/src/main/java/com/skypuddleindustries/slaps/MainActivity.java
+++ app/src/main/java/com/skypuddleindustries/slaps/MainActivity.java
@@ -2,10 +2,12 @@ package com.skypuddleindustries.slaps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.MediaStore;
 
 import java.io.IOException;
 
@@ -85,8 +87,48 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
     }
 
     @Override
-    public void onAudioFocusChange(int focusChange) {
+    public void onAudioFocusChange(int focusState) {
         //invoked when the audio focus of the system is updated
+        switch (focusState) {
+            case AudioManager.AUDIOFOCUS_GAIN:
+                // resume playback
+                if (mediaPlayer == null) initMediaPlayer();
+                else if (!mediaPlayer.isPlaying()) mediaPlayer.start();
+                mediaPlayer.setVolume(1.0f, 1.0f);
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS:
+                // lost focus for an unbounded mount of time: stop playback and release media player
+                if (mediaPlayer.isPlaying()) mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer = null;
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                // lost focus for a short time, but we have to stop playback
+                // we don't want to release the media player because
+                // playback is likely to resume
+                if (mediaPlayer.isPlaying()) mediaPlayer.pause();
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                // lost focus for a short time, but it's ok to keep playing more quietly
+                if (mediaPlayer.isPlaying()) mediaPlayer.setVolume(0.1f,0.1f);
+                break;
+
+        }
+    }
+
+    private boolean requestAudioFocus() {
+        audioManager =  (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int result = audioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            // focus gained
+            return true;
+        }
+        // could not gain focus
+        return false;
+    }
+
+    private boolean removeAudioFocus() {
+        return AudioManager.AUDIOFOCUS_REQUEST_GRANTED == audioManager.abandonAudioFocus(this);
     }
 
     public class LocalBinder extends Binder {
@@ -147,9 +189,11 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
         }
     }
 
-    private voide resumeMedia() {
+    private void resumeMedia() {
         if (!mediaPlayer.isPlaying()) {
             mediaPlayer.seekTo(resumePosition);
             mediaPlayer.start();
         }
     }
+
+    private AudioManager audioManager;
